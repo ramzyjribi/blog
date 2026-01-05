b0d44a941cf3596994e90d34a7538348798c6c22
unknown <ramzynet77@gmail.com>
2026-01-05 01:59:26 +0100

    fix baseurl commit

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -72,7 +72,7 @@ class ApiService {
 
   private constructor() {
     this.api = axios.create({
-      baseURL: '/api/v1',
+      baseURL: 'https://ramziblogapi.onrender.com/api/v1',
       headers: {
         'Content-Type': 'application/json'
       }
